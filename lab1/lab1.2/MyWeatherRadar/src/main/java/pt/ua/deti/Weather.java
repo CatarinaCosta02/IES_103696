a060fb032126e567a1a28659ca6c743ecb09a559
Catarina <catarinateves02ua.pt>
2022-09-27 22:02:23 +0100

    1.2

---
+++ lab1/lab1.2/MyWeatherRadar/src/main/java/pt/ua/deti/Weather.java
@@ -0,0 +1,59 @@
+package pt.ua.deti;
+
+/**
+ * Hello world!
+ *
+ */
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import pt.ua.deti.IpmaCityForecast; //may need to adapt package name
+import pt.ua.deti.IpmaService;
+
+public class Weather
+{
+    private static final int CITY_ID_AVEIRO = 1010500;
+
+    public static void main( String[] args ) {
+
+        // get a retrofit instance, loaded with the GSon lib to convert JSON into objects
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.ipma.pt/open-data/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // create a typed interface to use the remote API (a client)
+        IpmaService service = retrofit.create(IpmaService.class);
+        // prepare the call to remote endpoint
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(CITY_ID_AVEIRO);
+
+        //to receive the city code as a parameter in the command line and print the forecast information
+
+        if(args.length() > 0){
+
+            int code = 
+        }
+
+        try {
+            Response<IpmaCityForecast> apiResponse = callSync.execute();
+            IpmaCityForecast forecast = apiResponse.body();
+
+            if (forecast != null) {
+                CityForecast firstDay = forecast.getData().listIterator().next();
+
+                System.out.printf( "max temp for %s is %4.1f %n",
+                        firstDay.getForecastDate(),
+                        Double.parseDouble(firstDay.getTMax()));
+            } else {
+                System.out.println( "No results for this request!");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+
+    }
+
+}
