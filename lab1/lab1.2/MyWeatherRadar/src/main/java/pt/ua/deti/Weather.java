c5643196c470825d4facd67813dcd13a57fd538f
Catarina <catarinateves02ua.pt>
2022-09-28 01:07:02 +0100

    lala

--- lab1/lab1.2/MyWeatherRadar/src/main/java/pt/ua/deti/Weather.java
+++ lab1/lab1.2/MyWeatherRadar/src/main/java/pt/ua/deti/Weather.java
@@ -14,7 +14,9 @@ import pt.ua.deti.IpmaService;
 
 public class Weather
 {
-    private static final int CITY_ID_AVEIRO = 1010500;
+    //private static final int CITY_ID_AVEIRO = 1010500;
+
+    private static int code = 1010500;
 
     public static void main( String[] args ) {
 
@@ -27,13 +29,13 @@ public class Weather
         // create a typed interface to use the remote API (a client)
         IpmaService service = retrofit.create(IpmaService.class);
         // prepare the call to remote endpoint
-        Call<IpmaCityForecast> callSync = service.getForecastForACity(CITY_ID_AVEIRO);
+        //Call<IpmaCityForecast> callSync = service.getForecastForACity(CITY_ID_AVEIRO);
 
         //to receive the city code as a parameter in the command line and print the forecast information
 
-        if(args.length() > 0){
+        if(args.length > 0){
 
-            int code = 
+            code = Integer.parseInt(args[0]);
         }
 
         try {
@@ -42,6 +44,9 @@ public class Weather
 
             if (forecast != null) {
                 CityForecast firstDay = forecast.getData().listIterator().next();
+
+                System.out.println("Latitude: " + forecast.getData().listIterator().next().getLatitude()+ "\nLongitude" + forecast.getData().listIterator().next().getLongitude()
+                        + "\nmax temp for today: " + forecast.getData().listIterator().next().getTMax() + "Cº ," + "\nprobabilidade de precipitação: " +forecast.getData().listIterator().next().getPrecipitaProb() +"\n" + code);
 
                 System.out.printf( "max temp for %s is %4.1f %n",
                         firstDay.getForecastDate(),
